748d691fcb41fb22695242cf3ee43549b10e9940
brndalx <113251192+brandalx@users.noreply.github.com>
2023-12-14 02:11:17 -0500

    feat: initial app router, basic comps layout

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,7 +1,27 @@
+import Footer from "@/app/footer/Footer";
+import Header from "@/app/header/Header";
+import Home from "@/app/home/Home";
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const AppRouter = () => {
-  return <div>appRouter</div>;
+  return (
+    <>
+      <body className="h-full">
+        <main className="min-h-screen">
+          <Header />
+          <div className="flex-grow flex-1">
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Home />} />
+              </Routes>
+            </BrowserRouter>
+          </div>
+        </main>
+        <Footer />
+      </body>
+    </>
+  );
 };
 
 export default AppRouter;
