02f236703a519ebbbbc63c2f870c58d2bcb1b046
brndalx <113251192+brandalx@users.noreply.github.com>
2023-12-14 02:39:00 -0500

    feat: initial target ui

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div>
+        <div>
+          {/* left side */}
+          <div>input bill</div>
+
+          <div> tip selection</div>
+          <div> input ppl</div>
+        </div>
+        <div>
+          {/* right side */}
+          <div>display</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
