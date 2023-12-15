b875e49f6055d861eee239fb3969f4dc706c4d9e
brndalx <113251192+brandalx@users.noreply.github.com>
2023-12-14 22:30:07 -0500

    feat: calculate tip func

--- src/lib/calculateTip.ts
+++ src/lib/calculateTip.ts
@@ -3,5 +3,16 @@ export function calculateTip(
   numberOfPeople: number,
   tipPercent: number
 ): string | { tipAmount: number; total: number } {
-  return "";
+  if (bill <= 0 || numberOfPeople <= 1 || tipPercent < 0 || tipPercent > 100) {
+    return "Error: Invalid values";
+  }
+
+  const tipAmount = bill * (tipPercent / 100);
+  const total = bill + tipAmount;
+  const totalPerPerson = total / numberOfPeople;
+
+  return {
+    tipAmount: parseFloat(tipAmount.toFixed(2)),
+    total: parseFloat(totalPerPerson.toFixed(2)),
+  };
 }
